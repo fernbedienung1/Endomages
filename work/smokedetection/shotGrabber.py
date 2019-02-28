36d81b9a0505b7db1e64812c3087d9f4fdcbacf2
Maximilian Heichler <maximilian.heichler@newtec.de>
2019-02-28 09:59:39 +0100

    made it work

--- work/smokedetection/shotGrabber.py
+++ work/smokedetection/shotGrabber.py
@@ -3,38 +3,46 @@
 import cv2
 import time
 import argparse
+import shutil
 import os
 
 
-def main(vid, outFolder):
-    start = time.time()
-    
+def main(inFile, outFolder):
+    vid = cv2.VideoCapture(inFile)
+
     cnt = 0
+    start = time.time()
     while vid.isOpened():
-        print(start, end='\r') 
-        cnt += 1
         ret, frame = vid.read()
-        
-        if ret == False:
-            print ("CRITCAL ERROR IN READIN\'")
-            exit(ret)
+        if type(frame) == type(None):
+            break
 
         cv2.imshow('name', frame)
-        # cv2.imwrite("%s/shot_%d.jpg" % (outFolder, cnt), frame, cnt)    # this still throws erros
-        
-        
+
+        if start + 1 <= time.time():
+            cv2.imwrite("%s/shot_%d.jpg" % (outFolder, cnt), frame)
+            print("Writing:\t%s/%s_%d.jpg" % (outFolder,inFile, cnt))
+            cnt += 1
+            start = time.time()
+
+        if cv2.waitKey(5) & 0xFF == 27 or cv2.waitKey(5) & 0xFF == 113:
+            break			# thats ESC or 'q'
+
+    vid.release()
 
 
+# This makes only the Parsing, actual "inteligence" happens above
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='creats screenshots every second and writs them to given Folder')
     parser.add_argument("video", help="InputVideo")
-    parser.add_argument("output", help="Foldername for Screenshots", default='screenshots')
+    parser.add_argument("-o", "--output", help="specify output directory name for Resulting Video [MP4]", default='Screenshots')
 
     args = parser.parse_args()
-    inFile = cv2.VideoCapture(args.video)
 
-    if os.path.exists(args.output):
-        os.rmdir(args.output)
-        
+    if os.path.exists(args.output):     # remove the folder if i was lazy
+        shutil.rmtree(args.output)
+
     os.mkdir(args.output)
-    main(inFile, args.output)
+    main(args.video, args.output)
+
+    cv2.destroyAllWindows()
