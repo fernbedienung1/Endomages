ba875474a99434371e3e4c14ade032dc4f289022
m <m@xps>
2019-03-06 13:27:58 +0100

    overwored the history script - brought more of the hists into the Thesis

--- work/scripts/hist.py
+++ work/scripts/hist.py
@@ -8,14 +8,11 @@ import csv
 # first and obligatory arguent is the picture
 parser = argparse.ArgumentParser()
 parser.add_argument("jpg", help="Input picture for Histogram")
-args = parser.parse_args()
-jpg = args.jpg
-# TODO - add switch for fileoutput / screenoutput
-    # actually thas no longer very important...
+parser.add_argument("-l", "--createLog", help="create csv logfile", type=str, nargs='?' )
+parser.add_argument("-o", "--outFile", help="save to file instead of Display Result", type=str, nargs='?' )
+parser.add_argument("-s", "--short", help="create combined Histogram instead of 3 singe channels", action="store_true")
 
-# more globals
-output = jpg + "_HIST.png"
-log = jpg + "_HIST.csv"
+args = parser.parse_args()
 
 
 def calcHist(bgr_pic):
@@ -44,62 +41,74 @@ def calcHist(bgr_pic):
     return hist_b, hist_g, hist_r
 
 
-def __showPlt(blue, green, red):
+def __plt(blue, green, red):
     scale = range(0, 255, 1)
     plt.plot(scale, blue, 'b.', scale, green, 'g.', scale, red, 'r.')
-    plt.savefig(output, bbox_inches='tight')
+    if args.outFile:
+        plt.savefig(args.outFile, bbox_inches='tight')
+    else:
+        plt.show()
 
 
-def __showHist(blue, green, red):
-    plt.figure(1)
-
-    # Blue
-    plt.subplot(311)
-    plt.hist(blue, color='b')
-    plt.plot()
-
-    # Green
-    plt.subplot(321)
-    plt.hist(green, color='g')
-    plt.plot()
-
-    # Red
-    plt.subplot(331)
-    plt.hist(red, color='r')
-    plt.plot()
-
-    # common stuff
-    plt.title(jpg)
-
-
-def __createLog(b, g, r):
+def __createLog(b, g, r, name):
     # This will write a file with the calculated history data 
     # will be necessary for the "OVERALL" - histogramm
 
-    with open(log , mode='w') as csv_f:
+    with open(name, mode='w') as csv_f:
         csvWrite= csv.writer(csv_f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
             
         csvWrite.writerow(b)
         csvWrite.writerow(g)
         csvWrite.writerow(r)
 
-    print("logFile Created:\t %s" % log )
+
+def __hist(b, g, r):
+    plt.title("Histograms of color channels")
+    yaxis= max(len(b), len(g), len(r))
+
+    blue = plt.subplot(311)
+    blue.set_xlabel("Intensity of Blue Pixels")
+    blue.set_ylabel("Count of Pixles")
+    # blue.set_title("Blue Channel")
+    blue.axis([0,255, 0, yaxis]) 
+    blue.hist(b, color='b', density=1)
+
+    green = plt.subplot(312)
+    green.set_xlabel("Intensity of Green Pixels")
+    green.set_ylabel("Count of Pixles")
+    # green.set_title("Green Channel")
+    green.axis([0,255, 0, yaxis])
+    green.hist(g, color='g', density=1)
+
+    red = plt.subplot(313)
+    red.set_xlabel("Intensity of Red Pixels")
+    red.set_ylabel("Count of Pixles")
+    # red.set_title("Red Channel")
+    red.axis([0,255, 0, yaxis]) 
+    red.hist(r, color='r', density=1)
+
+
+    if args.outFile:
+        plt.savefig(args.outFile, bbox_inches='tight')
+    else:
+        plt.show()
     
 
 def main():
-    img = cv2.imread(jpg)
+    img = cv2.imread(args.jpg)
 
     blue, green, red = calcHist(img)
 
-    __showPlt(blue, green, red)
-    __showHist(blue, green, red)
-    __createLog(blue, green, red)
+    if args.short:
+        __plt(blue, green, red)
+    else:
+        __hist(blue, green, red)
+
+    if args.createLog:
+        __createLog(blue, green, red, args.createLog)
+        print("logFile Created:\t %s" % args.createLog)
 
 
 if __name__ == '__main__':
     print("-----  Histogram  -----")
-    print("Input File:\t%s" % jpg)
-    print("OutputFile:\t%s" % output)
-    print("CSV File:\t%s" % log)
     main()
-    print("\n")
