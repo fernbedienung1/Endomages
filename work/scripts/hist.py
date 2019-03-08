1df9a5a0a612d1515ba8ee9403eebeaeb7656f49
Maximilian Heichler <maximilian.heichler@newtec.de>
2019-03-08 17:11:39 +0100

    Bisschen mathe und mehr bilder
    histogramme und deren verwendung in Fundamentals eingebracht.
    Neue struktur for DCP und Histogramm equalization.
    next up sollten bilder und Mathe und Code von DCP eingebaut werden....

--- work/scripts/hist.py
+++ work/scripts/hist.py
@@ -65,29 +65,35 @@ def __createLog(b, g, r, name):
 
 def __hist(b, g, r):
     plt.title("Histograms of color channels")
-    yaxis= max(len(b), len(g), len(r))
+    #yaxis= max(len(b), len(g), len(r))
+    yaxis= max(max(b), max(g), max(r))
 
     blue = plt.subplot(311)
     blue.set_xlabel("Intensity of Blue Pixels")
     blue.set_ylabel("Count of Pixles")
     # blue.set_title("Blue Channel")
     blue.axis([0,255, 0, yaxis]) 
-    blue.hist(b, color='b', density=1)
+#    blue.hist(b, color='b', histtype='stepfilled')
+    blue.plot(b, color='b')
+    blue.fill_between(0,b)
 
     green = plt.subplot(312)
     green.set_xlabel("Intensity of Green Pixels")
     green.set_ylabel("Count of Pixles")
     # green.set_title("Green Channel")
     green.axis([0,255, 0, yaxis])
-    green.hist(g, color='g', density=1)
+#    green.hist(g, color='g', histtype='stepfilled')
+    green.plot(g, color='g')
+    green.fill_between(0,g)
 
     red = plt.subplot(313)
     red.set_xlabel("Intensity of Red Pixels")
     red.set_ylabel("Count of Pixles")
     # red.set_title("Red Channel")
     red.axis([0,255, 0, yaxis]) 
-    red.hist(r, color='r', density=1)
-
+#    red.hist(r, color='r',histtype='stepfilled')
+    red.plot(r, color='r')
+    red.fill_between(0,r)
 
     if args.outFile:
         plt.savefig(args.outFile, bbox_inches='tight')
@@ -98,7 +104,9 @@ def __hist(b, g, r):
 def main():
     img = cv2.imread(args.jpg)
 
+    start = time.time()
     blue, green, red = calcHist(img)
+    print("time:\t%s" % (str(time.time()-start)))
 
     if args.short:
         __plt(blue, green, red)
@@ -111,6 +119,4 @@ def main():
 
 
 if __name__ == '__main__':
-    start = time.time()
     main()
-    print("time:\t%f" % time.time()-start)
