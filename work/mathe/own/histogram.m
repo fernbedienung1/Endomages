5a1a3008d471e0e36ea508bdd163a652ca3766b4
Maximilian Heichler <maximilian.heichler@newtec.de>
2019-04-03 17:33:45 +0200

    work on methodic and corrections until page 6 - Organe of Deutsch_axel.pdf

--- work/mathe/own/histogram.m
+++ work/mathe/own/histogram.m
@@ -1,17 +1,13 @@
 function [f,F,I_vals] = histogram(img)
-%    img = imread(imageName);
     % get dimension
     dim = size(img);
     % create monochrome Picture by average colors
     mono = sum(img,3)/dim(3);
     % get maximum intensity of monochrome image
-%    I_max = max(max(mono));
     % create scale (all intesities)
     I_vals = [1:1:255];
-
-    % Here happens the action
 
     % create histogram
     h = hist(mono(:), I_vals);
     f = h/length(mono(:));
-    F = cumsum(f);      % hihihihi cumsum 
+    F = cumsum(f);      % cumulated sum over f
